b17821f17f93ee692c39b17037f068ad5a764189
SongWatson <watson.song@gmail.com>
2020-06-04 20:38:06 +0800

    放开loginUser的roles

--- src/main/java/cn/watsontech/core/web/spring/security/LoginUser.java
+++ src/main/java/cn/watsontech/core/web/spring/security/LoginUser.java
@@ -97,7 +97,6 @@ public abstract class LoginUser implements UserDetails {
     @Transient
     List<String> permissions = new ArrayList<>();
 
-    @JsonIgnore
     @ApiModelProperty(value = "用户角色")
     public List<String> getRoles() {
         return roles;
