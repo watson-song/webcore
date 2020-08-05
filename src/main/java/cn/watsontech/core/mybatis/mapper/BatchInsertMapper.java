f603e80ce944c2a6af2b4657ef1493aff2c54611
SongWatson <watson.song@gmail.com>
2020-08-05 12:58:50 +0800

    添加 批量插入不能返回所有对象主键的bug 提示

--- src/main/java/cn/watsontech/core/mybatis/mapper/BatchInsertMapper.java
+++ src/main/java/cn/watsontech/core/mybatis/mapper/BatchInsertMapper.java
@@ -17,14 +17,23 @@ public interface BatchInsertMapper<T> {
      * 批量插入，忽略插入异常；
      * 支持批量插入的数据库可以使用，例如MySQL,H2等，另外该接口限制实体包含`id`属性并且必须为自增列
      *
+     * 注意！！！：该方法当前存在bug，不能返回所有批量插入实体主键，仅能返回第一个实体主键，请谨慎使用
      * @param recordList
-     * @return
+     * @return 成功条目数量
      */
     @Options(useGeneratedKeys = true, keyProperty = "id")
     @InsertProvider(type = BatchInsertProvider.class, method = "dynamicSQL")
     int insertListIgnoreConflict(List<T> recordList);
 
 
+    /**
+     * 批量插入，忽略插入异常；
+     * 支持批量插入的数据库可以使用，例如MySQL,H2等，另外该接口限制实体包含`id`属性并且必须为自增列
+     *
+     * 注意！！！：该方法当前存在bug，不能返回所有批量插入实体主键，仅能返回第一个实体主键，请谨慎使用
+     * @param batchInsertModel 要插入的实体数据
+     * @return 成功条目数量
+     */
     @Options(useGeneratedKeys = true, keyProperty = "id")
     @InsertProvider(type = BatchInsertProvider.class, method = "dynamicSQL")
     int insertSelectiveListIgnoreConflict(BatchInsertModel<T> batchInsertModel);
