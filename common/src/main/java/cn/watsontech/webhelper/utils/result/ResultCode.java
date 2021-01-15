13917f107f09b0651d43c2716f102e6573d4cf47
SongWatson <watson.song@gmail.com>
2021-01-15 10:54:17 +0800

    remove useless Result Object

--- common/src/main/java/cn/watsontech/webhelper/utils/result/ResultCode.java
+++ common/src/main/java/cn/watsontech/webhelper/utils/result/ResultCode.java
@@ -1,12 +1,12 @@
-package cn.watsontech.webhelper.utils.result;
-
-/**
- * Created by Watson on 2019/12/20.
- */
-public class ResultCode {
-    public static final int SUCCESS = 200;
-    public static final int BAD_REQUEST = 400;
-    public static final int NOT_FOUND = 404;
-    public static final int NOT_AUTH = 401;
-    public static final int INTERNAL_ERROR = 500;
-}
+//package cn.watsontech.webhelper.utils.result;
+//
+///**
+// * Created by Watson on 2019/12/20.
+// */
+//public class ResultCode {
+//    public static final int SUCCESS = 200;
+//    public static final int BAD_REQUEST = 400;
+//    public static final int NOT_FOUND = 404;
+//    public static final int NOT_AUTH = 401;
+//    public static final int INTERNAL_ERROR = 500;
+//}
--- common/src/main/java/cn/watsontech/webhelper/utils/result/ResultPage.java
+++ common/src/main/java/cn/watsontech/webhelper/utils/result/ResultPage.java
@@ -1,14 +1,14 @@
-package cn.watsontech.webhelper.utils.result;
-
-import java.util.List;
-
-/**
- * Created by Watson on 2020/11/25.
- */
-public interface ResultPage<T> extends List {
-
-    List<T> getResult();
-    Number getStartRow();
-    Number getPageSize();
-    Number getTotal();
-}
+//package cn.watsontech.webhelper.utils.result;
+//
+//import java.util.List;
+//
+///**
+// * Created by Watson on 2020/11/25.
+// */
+//public interface ResultPage<T> extends List {
+//
+//    List<T> getResult();
+//    Number getStartRow();
+//    Number getPageSize();
+//    Number getTotal();
+//}
